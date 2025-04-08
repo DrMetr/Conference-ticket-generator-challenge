dfb35da1ba17acb2079cb8f6b100f93384d7d481
DrMetr <metronomedragon@gmail.com>
2025-04-08 16:05:22 +0300

    Layout a little done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            AlmostWhite:'hsl(252, 6%, 83%)',
+            LightGrey: 'hsl(245, 15%, 58%)',
+            MediumGray: 'hsl(245, 19%, 35%)',
+            DarkBlue: 'hsl(248, 70%, 10%)',
+            Orange: 'hsl(7, 88%, 67%)',
+            OrangeDarker: 'hsl(7, 71%, 60%)'
+        },
+        fontFamily: {
+            Inconsolata: ["Inconsolata", "sans-serif"],
+        },
+      },
+      plugins: [],
+    },
+  };
