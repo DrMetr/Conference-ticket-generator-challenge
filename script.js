44c6f59a11b5a721f4e021f9e5aa9f7c46e94d51
DrMetr <metronomedragon@gmail.com>
2025-04-21 12:22:32 +0300

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const name = document.querySelector("#FullName")
-const email = document.querySelector("#email")
-const github = document.querySelector("#GitHub")
-const avatar = document.querySelector('#avatar')
+const button = document.querySelector('button')
+const form = document.querySelector('.form_section')
+const ticket = document.querySelector('.ticket_section')
+//input:
+const fullName = document.querySelector('#FullName')
+const email = document.querySelector('#email')
+const github = document.querySelector('#GitHub')
+
 const congratsName = document.querySelector('.name')
 const congratsEmail = document.querySelector('.email')
+
+const ticketName = document.querySelector('.name_ticket')
+const ticketGitHub = document.querySelector('.ticket_github')
+const ticketNumber = document.querySelector('.number')
+
+const ticketData = [ticketName, ticketNumber, ticketGitHub]
+const inputData = [fullName, email, github]
+
+button.addEventListener('click', evt => {
+    evt.preventDefault();
+    form.classList.add('hidden');
+    ticket.classList.remove('hidden')
+    fillData(ticketData, inputData);
+})
+
+function fillData(position, data){
+    congratsName.innerHTML = fullName.value;
+    congratsEmail.innerHTML = email.value;
+    
+    ticketName.innerHTML = fullName.value;
+    ticketGitHub.innerHTML = github.value;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ tailwind.config = {
         colors: {
             AlmostWhite:'hsl(252, 6%, 83%)',
             LightGrey: 'hsl(245, 15%, 58%)',
-            MediumGray: 'hsl(245, 19%, 35%)',
+            MediumGray: 'hsla(245, 58.70%, 76.30%, 0.16)',
             DarkBlue: 'hsl(248, 70%, 10%)',
             Orange: 'hsl(7, 88%, 67%)',
             OrangeDarker: 'hsl(7, 71%, 60%)'
