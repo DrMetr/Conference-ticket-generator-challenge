cd1d30acf8e90952d3943c0a1522f3304b2cf0f9
DrMetr <metronomedragon@gmail.com>
2025-04-22 16:14:16 +0300

    Finishing

--- script.js
+++ script.js
@@ -17,13 +17,17 @@ const ticketData = [ticketName, ticketNumber, ticketGitHub]
 const inputData = [fullName, email, github]
 
 const emailRegExp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const githubRegExp = /\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))/gi;
 
 button.addEventListener('click', evt => {
     evt.preventDefault();
-    if ()
-    form.classList.add('hidden');
-    ticket.classList.remove('hidden')
-    fillData(ticketData, inputData);
+    if (checkInput()){
+        form.classList.add('hidden');
+        ticket.classList.remove('hidden');
+        fillData(ticketData, inputData);
+    } else {
+        return
+    }
 })
 
 function fillData(position, data){
@@ -32,14 +36,47 @@ function fillData(position, data){
     
     ticketName.innerHTML = fullName.value;
     ticketGitHub.innerHTML = github.value;
+    ticketNumber.innerHTML = "#" + Math.random().toString().slice(2,7);
+}
+
+function validateName(nameInput){
+    if (nameInput.value != ''){
+        document.querySelector('.errorName').classList.add('hidden');
+    } else {
+        document.querySelector('.errorName').classList.remove('hidden')
+    }
 }
 
 function validateEmail(emailInput){
-    return emailRegExp.test(String(emailInput).toLowerCase());
+    if(emailRegExp.test(String(emailInput).toLowerCase())){
+        document.querySelector('.errorEmail').classList.add('hidden')
+    } else {
+        document.querySelector('.errorEmail').classList.remove('hidden')
+    }
+}
+
+function validateGitHub(githubInput){
+    if(githubRegExp.test(String(githubInput).toLowerCase())){
+        document.querySelector('.errorGitHub').classList.add('hidden')
+    } else {
+        document.querySelector('.errorGitHub').classList.remove('hidden')
+    }
 }
 
 function checkInput(){
-    if (validateEmail(email.value)){
-        
+    let counter = 0;
+    validateName(fullName);
+    validateEmail(email.value);
+    validateGitHub(github.value);
+    let errors = document.querySelectorAll('.error');
+    errors.forEach(error => {
+        if (!error.classList.contains('hidden')){
+            counter += 1;
+        }
+    });
+    if (counter == 0){
+        return true
+    } else {
+        return false
     }
 }
